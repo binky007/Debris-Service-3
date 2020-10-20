7abb9c466f9b7c1734e91d7fbe91c6ffb2e29a52
binky007 <72167343+binky007@users.noreply.github.com>
2020-10-20 17:47:29 +0100

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -2,18 +2,26 @@ local DebrisService = require(script.Debris)
 
 local Part1 = Instance.new("Part")
 Part1.Parent = workspace
+Part1.Anchored = true
 
 local Connection
 Connection = Part1.Touched:Connect(function(hit)
 	print("Apple")
 end)
 
-DebrisService:AddItem(Connection, 8)
+DebrisService:AddItem(Connection, 2)
 
 print(DebrisService:GetAllDebris()) -- Will print just one as there is only one entry so far
 
 print(DebrisService:GetDebris(Connection)) -- WIll print all the information about the specific instance / table / connection in the debris table
 
-DebrisService:AddItem(Part1, 10)
+DebrisService:AddItem(Part1, 7, {
+	Duration = 3, -- Needs to be an integer
+	Tweeninfo =  TweenInfo.new(1), -- Needs to be a TweenInfo
+	Goals = {
+		['CFrame'] = CFrame.new(0,10,10),
+		Transparency = 1,
+	} -- Needs to be a table
+})
 
 print(DebrisService:GetAllDebris()) -- Will print two as there is now two entries
